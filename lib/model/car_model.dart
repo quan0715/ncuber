57e1cd5fd10468feb13096de1c52341fc3705bc5
quan0715 <quan787887@gmail.com>
2023-05-17 11:22:47 +0800

    card view

--- lib/model/car_model.dart
+++ lib/model/car_model.dart
@@ -3,17 +3,72 @@ import 'dart:ffi';
 /// Type: model Class
 /// Descriptions: 併車的(房間)屬性
 /// Author: Staler2019(Github.com)
+
+class CarStatus {
+  /// 併車房間狀態
+  // static String notReady = "招募中";
+  // static String inProgress = "已發車";
+  // static String full = "已滿座";
+  // static String end = "已結束";
+  final String statusName;
+  final int statusColor;
+  const CarStatus({required this.statusName, required this.statusColor});
+  factory CarStatus.notReady() => const CarStatus(
+    statusName: '招募中',
+    statusColor: 0XFF0066FF,
+  );
+  factory CarStatus.inProgress() => const CarStatus(
+    statusName: '已發車',
+    statusColor: 0XFF0FC100,
+  );
+  factory CarStatus.full() => const CarStatus(
+    statusName: '已滿座',
+    statusColor: 0XFFFF9F0F,
+  );
+  factory CarStatus.end() => const CarStatus(
+    statusName: '已結束',
+    statusColor: 0XFFFF2C0F,
+  );
+}
+
 class CarModel {
-  late String roomTitle; // 標題
-  late String status; // 現在狀態
-  late int launchPersonUid; // 發起人uid
-  late String remark; // 備註
-  late DateTime startTime; // 出發時間
-  late String startLoc; // 集合地點
-  late DateTime endTime; // 預期到達時間
-  late String endLoc; // 目的地
-  late int personsNumLimit; // 人數上限
-  late int totalExceptCost; // 搭乘總花費
-  late String genderLimit; // 性別限制
-  late Float ratingStandard; // 乘客最低分數
+  String? roomTitle; // 標題
+  CarStatus? status; // 現在狀態
+  int? launchPersonUid; // 發起人uid
+  String? remark; // 備註
+  DateTime? startTime; // 出發時間
+  String? startLoc; // 集合地點
+  DateTime? endTime; // 預期到達時間
+  String? endLoc; // 目的地
+  int? personsNumLimit; // 人數上限
+  int? totalExceptCost; // 搭乘總花費
+  String? genderLimit; // 性別限制
+  Float? ratingStandard; // 乘客最低分數
+  CarModel(
+      {this.roomTitle,
+      this.status,
+      this.launchPersonUid,
+      this.remark,
+      this.startTime,
+      this.startLoc,
+      this.endTime,
+      this.endLoc,
+      this.personsNumLimit,
+      this.totalExceptCost,
+      this.genderLimit,
+      this.ratingStandard});
+  factory CarModel.create(int launchPersonUid) => CarModel(
+        roomTitle: 'NCUBER new',
+        status: CarStatus.notReady(),
+        launchPersonUid: launchPersonUid,
+        remark: '',
+        startTime: DateTime.now(),
+        startLoc: '中央大學正門口',
+        endTime: DateTime.now(),
+        endLoc: '桃園高鐵站',
+        personsNumLimit: 4,
+        totalExceptCost: 0,
+        ratingStandard: const Float(),
+        genderLimit: '',
+      );
 }
--- lib/view/dashboard_ncuber.dart
+++ lib/view/dashboard_ncuber.dart
@@ -1,12 +1,156 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:ncuber/model/car_model.dart';
+import 'package:ncuber/view/dashboard_view.dart';
+import 'package:provider/provider.dart';
+
+class DashboardViewModel extends ChangeNotifier {
+  List<CarModel> allCarPool = [
+    CarModel.create(1)..status = CarStatus.notReady(),
+    CarModel.create(2)..status = CarStatus.inProgress(),
+    CarModel.create(3)..status = CarStatus.full(),
+  ];
+}
 
 class DashboardNcUber extends StatelessWidget {
-const DashboardNcUber({ Key? key }) : super(key: key);
+  const DashboardNcUber({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
-    return const Center(
-      child: Text('Dashboard'),
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<DashboardViewModel>(
+      create: (context) => DashboardViewModel(),
+      child: Consumer<DashboardViewModel>(
+          builder: (context, viewModel, child) => Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Center(
+                      child: Text('所有拼車',
+                          style: Theme.of(context).textTheme.titleLarge),
+                    ),
+                    viewModel.allCarPool.isEmpty
+                        ? Expanded(
+                            flex: 1,
+                            child: Column(
+                              children: [
+                                const Text('目前沒有任何拼車'),
+                                ElevatedButton(
+                                  onPressed: () {
+                                    Navigator.pushNamed(context, '/create');
+                                  },
+                                  child: const Text('發起拼車'),
+                                ),
+                              ],
+                            ))
+                        : Expanded(
+                            child: ListView.builder(
+                              itemCount: viewModel.allCarPool.length,
+                              itemBuilder: (context, index) {
+                                final carPool = viewModel.allCarPool[index];
+                                return CarPoolCard(carPoolData: carPool);
+                              },
+                            ),
+                          ),
+                  ],
+                ),
+              )),
+    );
+  }
+}
+
+class CarPoolCard extends StatefulWidget {
+  const CarPoolCard({super.key, required this.carPoolData});
+  final CarModel carPoolData;
+  @override
+  State<CarPoolCard> createState() => _CarPoolCardState();
+}
+
+class _CarPoolCardState extends State<CarPoolCard> {
+  Widget getTimeLocDisplay({required DateTime time, required String loc}) {
+    final formatter = DateFormat('yyyy-MM-dd HH:mm');
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(loc,
+            style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                fontWeight: FontWeight.bold,
+                color: Theme.of(context).colorScheme.primary)),
+        Text(formatter.format(time),
+            style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                fontWeight: FontWeight.bold,
+                color: Theme.of(context).colorScheme.secondary)),
+      ],
+    );
+  }
+
+  Widget getChip() {
+    Color color = Color(widget.carPoolData.status!.statusColor);
+    return Chip(
+      visualDensity: VisualDensity.compact,
+      backgroundColor: color.withOpacity(0.15),
+      avatar: CircleAvatar(backgroundColor: color, radius: 5,),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      side: BorderSide(color: color.withOpacity(0.2), width: 1),
+      labelStyle: Theme.of(context).textTheme.labelSmall!.copyWith(
+        fontWeight: FontWeight.bold,
+        color: color
+      ),
+      label: Text(widget.carPoolData.status!.statusName));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 10),
+      child: Card(
+        elevation: 3,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              getChip(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // car pool title display
+                  Text(widget.carPoolData.roomTitle ?? '',
+                      style: Theme.of(context)
+                          .textTheme
+                          .titleLarge!
+                          .copyWith(fontWeight: FontWeight.bold)),
+                  // car pool persons num display
+                  Text('0 / ${widget.carPoolData.personsNumLimit}',
+                      style: Theme.of(context)
+                          .textTheme
+                          .titleMedium!
+                          .copyWith(fontWeight: FontWeight.bold)),
+                ],
+              ),
+              const Divider(),
+              // Time display
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // car pool start time display
+                  getTimeLocDisplay(
+                      time: widget.carPoolData.startTime!,
+                      loc: widget.carPoolData.startLoc!),
+                  const Icon(Icons.arrow_forward),
+                  // car pool end time display
+                  getTimeLocDisplay(
+                      time: widget.carPoolData.endTime!,
+                      loc: widget.carPoolData.endLoc!),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
